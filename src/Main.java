7a8045dde7f07865f606e9d6e6519ac95c825fe6
Mileto Almeida <almeidamileto@gmail.com>
2025-01-15 20:00:21 -0300

    Primeiro dia

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -9,58 +10,61 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Carrega variáveis de ambiente de um arquivo .env e as utiliza no programa
- * Programa feito para fins de estudo do #7DaysOfCode da Alura para desenvolver uma aplicação que consome uma API de filmes.
- * A API utilizada aqui é a do TMDB (The Movie Database) e é necessário um token de leitura da API para utilizá-la.
+ * O método main é responsável por fazer a requisição a API do The Movie DB
+ * e imprimir o resultado no console.
+ * Ele carrega a chave da API de um arquivo .env para proteção da chave individual e faz a requisição a API
+ * com a chave e a busca desejada.
+ *
+ * @throws URISyntaxException Exceção lançada quando a URI passada para o HttpRequest não é válida
+ * @throws IOException Exceção lançada quando ocorre um erro de I/O
+ * @throws InterruptedException Exceção lançada quando a thread é interrompida
  */
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Map<String, String> env = loadEnv(".env"); // Carrega variáveis de ambiente de um arquivo .env
-        String apiKey = env.get("API_KEY");
-        String apiToken = env.get("API_TOKEN");
-        URI uriTMDB = URI.create("https://api.themoviedb.org/3/movie/550?api_key=" + apiKey); // URI da API do TMDB
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+        Map<String, String> env = loadEnv(".env");
+        String key = env.get("API_KEY");
 
-        if (apiKey == null || apiToken == null) {
-            System.out.println("API_KEY or API_TOKEN is not set"); // Verifica se as variáveis de ambiente
-            // foram carregadas corretamente e se não, exibe uma mensagem de erro
+        if (key == null) {
+            System.out.println("API_KEY não encontrada");
             return;
         }
 
-        //Cria uma nova instância de HttpClient e faz uma requisição GET para a URI da API do TMDB
+
+        String search = "top_rated";
+        int pagina = 1;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(uriTMDB)
+                .uri(new URI("https://api.themoviedb.org/3/movie/" + search + "?api_key=" + key + "&language=pt-BR&page=" + pagina)) //https://api.themoviedb.org/3/movie/top_rated?api_key=8e479da2b3a497455374bff051aab732&language=pt-BR&page=1
+                .GET()
                 .build();
 
-        //Recebe a resposta da requisição e exibe o JSON retornado
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String json = response.body();
 
-        System.out.println("Resposta: " + json);
-
+        System.out.println(json);
     }
 
     /**
-     * Carrega variáveis de ambiente de um arquivo .env
+     * Carrega as variáveis de ambiente de um arquivo .env
      *
      * @param filePath Caminho do arquivo .env
      * @return Map com as variáveis de ambiente
      */
-
     private static Map<String, String> loadEnv(String filePath) {
         Map<String, String> env = new HashMap<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader bf = new BufferedReader(new FileReader(filePath))) {
             String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split("=", 2);
-                if (parts.length == 2) {
-                    env.put(parts[0], parts[1]);
-                }
+            while ((line = bf.readLine()) != null) {
+                String[] parts = line.split("=");
+                env.put(parts[0], parts[1]);
             }
+            return env;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        return env;
     }
+
 }
